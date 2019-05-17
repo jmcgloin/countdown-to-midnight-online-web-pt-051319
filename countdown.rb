fc4557e2e9676ecd88b8ae4617c430b93c6a375c
Jason McGloin <jason.mcgloin@gmail.com>
2019-05-17 02:16:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(time)
   end
   'HAPPY NEW YEAR!'
 end
+
+def countdown_with_sleep(time)
+  i = time
+  while i > 0 do
+    puts "#{i} SECOND(S)!"
+    i -= 1
+    sleep(1000)
+  end
+  'HAPPY NEW YEAR!'
+end
